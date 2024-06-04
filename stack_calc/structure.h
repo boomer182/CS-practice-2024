4be0c3e395cc07e1000dc2f26275833f2a9ca35a
Kirill Dyomin <styofficial31@gmail.com>
2024-06-05 00:39:00 +0300

    add project stack_calc

---
+++ stack_calc/structure.h
@@ -0,0 +1,15 @@
+#ifndef STRUCTURE_H_INCLUDED
+#define STRUCTURE_H_INCLUDED
+
+typedef struct node
+{
+    double data;
+    struct node *next;
+} node;
+
+typedef struct stack
+{
+    node *top;
+} stack;
+
+#endif // STRUCTURE_H_INCLUDED
